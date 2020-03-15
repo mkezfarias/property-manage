b0a56eb01cfc1b09092bcdd264993c48a769ac25
sarah <sarachamorro@gmail.com>
2020-03-15 10:54:46 -0300

    Add house scrap to loop

--- scrapper/seeds.rb
+++ scrapper/seeds.rb
@@ -5,8 +5,9 @@ properties = []
 
 # each iteration adds 50 properties
 # 25 houses and 25 flats
-(1..5).each do |i|
+(1..2).each do |i|
   properties += scrapper(1, i)
+  properties += scrapper(0, i)
 end
 
 p properties.count
@@ -14,7 +15,8 @@ p properties.count
 properties.each do |property|
  new_property = Property.new
   property.each do |key, val|
-    new_property.key = val
+   new_property.key = val
+   #p val
   end
   new_property.save
 end
